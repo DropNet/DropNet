494bfc7afebf5f1aa812e29f2cd0a6d03f3fe9d1
dkarzon <dkarzon@tboda.com>
2010-08-12 18:41:31 +1000

    Added some Method comments and some Move/Copy functions (both Sync and Async)

--- DropNet/Client/Files.Async.cs
+++ DropNet/Client/Files.Async.cs
@@ -9,6 +9,12 @@ namespace DropNet
     public partial class DropNetClient
     {
 
+        /// <summary>
+        /// Gets MetaData for a File or Folder. For a folder this includes its contents. For a file, this includes details such as file size.
+        /// </summary>
+        /// <param name="path">The path of the file or folder</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns></returns>
         public void GetMetaDataAsync(string path, Action<RestResponse<MetaData>> callback)
         {
             if (!path.StartsWith("/")) path = "/" + path;
@@ -26,6 +32,12 @@ namespace DropNet
         }
 
         //TODO - Make class for this to return (instead of just a byte[])
+        /// <summary>
+        /// Downloads a File from dropbox given the path
+        /// </summary>
+        /// <param name="path">The path of the file to download</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns></returns>
         public void GetFile(string path, Action<RestResponse> callback)
         {
             if (!path.StartsWith("/")) path = "/" + path;
@@ -42,17 +54,40 @@ namespace DropNet
             _restClient.ExecuteAsync(request, callback);
         }
 
-#if FRAMEWORK
-        public void UploadFile(string path, FileInfo localFile, Action<RestResponse> callback)
-        {
-            if (!path.StartsWith("/")) path = "/" + path;
+#if WINDOWS_PHONE
+        //Exclude for now...
+#else
 
+        /// <summary>
+        /// Uploads a File to Dropbox from the local file system to the specified folder
+        /// </summary>
+        /// <param name="path">The path of the folder to upload to</param>
+        /// <param name="localFile">The local file to upload</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns></returns>
+        public void UploadFileAsync(string path, FileInfo localFile, Action<RestResponse> callback)
+        {
             //Get the file stream
             byte[] bytes = null;
             FileStream fs = new FileStream(localFile.FullName, FileMode.Open, FileAccess.Read);
             BinaryReader br = new BinaryReader(fs);
             long numBytes = localFile.Length;
             bytes = br.ReadBytes((int)numBytes);
+
+            UploadFileAsync(path, localFile.Name, bytes, callback);
+        }
+
+        /// <summary>
+        /// Uploads a File to Dropbox given the raw data.
+        /// </summary>
+        /// <param name="path">The path of the folder to upload to</param>
+        /// <param name="filename">The Name of the file to upload to dropbox</param>
+        /// <param name="fileData">The file data</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns>True on success</returns>
+        public void UploadFileAsync(string path, string filename, byte[] fileData, Action<RestResponse> callback)
+        {
+            if (!path.StartsWith("/")) path = "/" + path;
 
             //This has to be here as Dropbox change their base URL between calls
             _restClient.BaseUrl = "http://api-content.dropbox.com";
@@ -63,9 +98,9 @@ namespace DropNet
             request.AddParameter("version", _version, ParameterType.UrlSegment);
             request.AddParameter("path", path, ParameterType.UrlSegment);
             //Need to add the "file" parameter with the file name
-            request.AddParameter("file", localFile.Name);
+            request.AddParameter("file", filename);
 
-            request.AddFile(new FileParameter { Data = bytes, FileName = localFile.Name, ParameterName = "file" });
+            request.AddFile(new FileParameter { Data = fileData, FileName = filename, ParameterName = "file" });
 
             _restClient.ExecuteAsync(request, callback);
         }
@@ -75,6 +110,7 @@ namespace DropNet
         /// Deletes the file or folder from dropbox with the given path
         /// </summary>
         /// <param name="path">The Path of the file or folder to delete.</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
         /// <returns></returns>
         public void DeleteAsync(string path, Action<RestResponse> callback)
         {
@@ -89,6 +125,60 @@ namespace DropNet
             request.AddParameter("version", _version, ParameterType.UrlSegment);
 
             request.AddParameter("path", path);
+            request.AddParameter("root", "dropbox");
+
+            _restClient.ExecuteAsync(request, callback);
+        }
+
+        /// <summary>
+        /// Copies a file or folder on Dropbox
+        /// </summary>
+        /// <param name="fromPath">The path to the file or folder to copy</param>
+        /// <param name="toPath">The path to where the file or folder is getting copied</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns>True on success</returns>
+        public void Copy(string fromPath, string toPath, Action<RestResponse> callback)
+        {
+            if (!fromPath.StartsWith("/")) fromPath = "/" + fromPath;
+            if (!toPath.StartsWith("/")) toPath = "/" + toPath;
+
+            //This has to be here as Dropbox change their base URL between calls
+            _restClient.BaseUrl = "http://api.dropbox.com";
+            _restClient.Authenticator = new OAuthAuthenticator(_restClient.BaseUrl, _apiKey, _appsecret, _userLogin.Token, _userLogin.Secret);
+
+            var request = new RestRequest(Method.GET);
+            request.Resource = "{version}/fileops/copy";
+            request.AddParameter("version", _version, ParameterType.UrlSegment);
+
+            request.AddParameter("from_path", fromPath);
+            request.AddParameter("to_path", toPath);
+            request.AddParameter("root", "dropbox");
+
+            _restClient.ExecuteAsync(request, callback);
+        }
+
+        /// <summary>
+        /// Moves a file or folder on Dropbox
+        /// </summary>
+        /// <param name="fromPath">The path to the file or folder to move</param>
+        /// <param name="toPath">The path to where the file or folder is getting moved</param>
+        /// <param name="callback">The callback Action to perform on completion</param>
+        /// <returns>True on success</returns>
+        public void Move(string fromPath, string toPath, Action<RestResponse> callback)
+        {
+            if (!fromPath.StartsWith("/")) fromPath = "/" + fromPath;
+            if (!toPath.StartsWith("/")) toPath = "/" + toPath;
+
+            //This has to be here as Dropbox change their base URL between calls
+            _restClient.BaseUrl = "http://api.dropbox.com";
+            _restClient.Authenticator = new OAuthAuthenticator(_restClient.BaseUrl, _apiKey, _appsecret, _userLogin.Token, _userLogin.Secret);
+
+            var request = new RestRequest(Method.GET);
+            request.Resource = "{version}/fileops/move";
+            request.AddParameter("version", _version, ParameterType.UrlSegment);
+
+            request.AddParameter("from_path", fromPath);
+            request.AddParameter("to_path", toPath);
             request.AddParameter("root", "dropbox");
 
             _restClient.ExecuteAsync(request, callback);
--- DropNet/Client/Files.Sync.cs
+++ DropNet/Client/Files.Sync.cs
@@ -11,6 +11,11 @@ namespace DropNet
     public partial class DropNetClient
     {
 
+        /// <summary>
+        /// Gets MetaData for a File or Folder. For a folder this includes its contents. For a file, this includes details such as file size.
+        /// </summary>
+        /// <param name="path">The path of the file or folder</param>
+        /// <returns></returns>
         public MetaData GetMetaData(string path)
         {
             if (!path.StartsWith("/")) path = "/" + path;
@@ -30,6 +35,11 @@ namespace DropNet
         }
 
         //TODO - Make class for this to return (instead of just a byte[])
+        /// <summary>
+        /// Downloads a File from dropbox given the path
+        /// </summary>
+        /// <param name="path">The path of the file to download</param>
+        /// <returns>The files raw bytes</returns>
         public byte[] GetFile(string path)
         {
             if (!path.StartsWith("/")) path = "/" + path;
@@ -48,16 +58,34 @@ namespace DropNet
             return responseData;
         }
 
+        /// <summary>
+        /// Uploads a File to Dropbox from the local file system to the specified folder
+        /// </summary>
+        /// <param name="path">The path of the folder to upload to</param>
+        /// <param name="localFile">The local file to upload</param>
+        /// <returns>True on success</returns>
         public bool UploadFile(string path, FileInfo localFile)
         {
-            if (!path.StartsWith("/")) path = "/" + path;
-
             //Get the file stream
             byte[] bytes = null;
             FileStream fs = new FileStream(localFile.FullName, FileMode.Open, FileAccess.Read);
             BinaryReader br = new BinaryReader(fs);
             long numBytes = localFile.Length;
             bytes = br.ReadBytes((int)numBytes);
+
+            return UploadFile(path, localFile.Name, bytes);
+        }
+
+        /// <summary>
+        /// Uploads a File to Dropbox given the raw data.
+        /// </summary>
+        /// <param name="path">The path of the folder to upload to</param>
+        /// <param name="filename">The Name of the file to upload to dropbox</param>
+        /// <param name="fileData">The file data</param>
+        /// <returns>True on success</returns>
+        public bool UploadFile(string path, string filename, byte[] fileData)
+        {
+            if (!path.StartsWith("/")) path = "/" + path;
 
             //This has to be here as Dropbox change their base URL between calls
             _restClient.BaseUrl = "http://api-content.dropbox.com";
@@ -68,9 +96,9 @@ namespace DropNet
             request.AddParameter("version", _version, ParameterType.UrlSegment);
             request.AddParameter("path", path, ParameterType.UrlSegment);
             //Need to add the "file" parameter with the file name
-            request.AddParameter("file", localFile.Name);
+            request.AddParameter("file", filename);
 
-            request.AddFile(new FileParameter { Data = bytes, FileName = localFile.Name, ParameterName = "file" });
+            request.AddFile(new FileParameter { Data = fileData, FileName = filename, ParameterName = "file" });
 
             var response = _restClient.Execute(request);
 
@@ -95,6 +123,62 @@ namespace DropNet
             request.AddParameter("version", _version, ParameterType.UrlSegment);
 
             request.AddParameter("path", path);
+            request.AddParameter("root", "dropbox");
+
+            var response = _restClient.Execute(request);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Copies a file or folder on Dropbox
+        /// </summary>
+        /// <param name="fromPath">The path to the file or folder to copy</param>
+        /// <param name="toPath">The path to where the file or folder is getting copied</param>
+        /// <returns>True on success</returns>
+        public bool Copy(string fromPath, string toPath)
+        {
+            if (!fromPath.StartsWith("/")) fromPath = "/" + fromPath;
+            if (!toPath.StartsWith("/")) toPath = "/" + toPath;
+
+            //This has to be here as Dropbox change their base URL between calls
+            _restClient.BaseUrl = "http://api.dropbox.com";
+            _restClient.Authenticator = new OAuthAuthenticator(_restClient.BaseUrl, _apiKey, _appsecret, _userLogin.Token, _userLogin.Secret);
+
+            var request = new RestRequest(Method.GET);
+            request.Resource = "{version}/fileops/copy";
+            request.AddParameter("version", _version, ParameterType.UrlSegment);
+
+            request.AddParameter("from_path", fromPath);
+            request.AddParameter("to_path", toPath);
+            request.AddParameter("root", "dropbox");
+
+            var response = _restClient.Execute(request);
+
+            return response.StatusCode == System.Net.HttpStatusCode.OK;
+        }
+
+        /// <summary>
+        /// Moves a file or folder on Dropbox
+        /// </summary>
+        /// <param name="fromPath">The path to the file or folder to move</param>
+        /// <param name="toPath">The path to where the file or folder is getting moved</param>
+        /// <returns>True on success</returns>
+        public bool Move(string fromPath, string toPath)
+        {
+            if (!fromPath.StartsWith("/")) fromPath = "/" + fromPath;
+            if (!toPath.StartsWith("/")) toPath = "/" + toPath;
+
+            //This has to be here as Dropbox change their base URL between calls
+            _restClient.BaseUrl = "http://api.dropbox.com";
+            _restClient.Authenticator = new OAuthAuthenticator(_restClient.BaseUrl, _apiKey, _appsecret, _userLogin.Token, _userLogin.Secret);
+
+            var request = new RestRequest(Method.GET);
+            request.Resource = "{version}/fileops/move";
+            request.AddParameter("version", _version, ParameterType.UrlSegment);
+
+            request.AddParameter("from_path", fromPath);
+            request.AddParameter("to_path", toPath);
             request.AddParameter("root", "dropbox");
 
             var response = _restClient.Execute(request);
