5ac7899fc509f0d7a14d3b279995bab6aead8d5b
kg4lod <jf@atlalabs.com>
2011-02-04 20:17:57 -0800

    Added the Result object model. The Result object is returned from file operations. It magically turns itself into a boolean so that you can test for success without code modification, but if you want more information it magically turns itself into a System.Net.HttpStatusCode object.
    NO EXPLICIT CASTING NEEDED!
    
    Result theResult;
    if (theResult) { //VALID! (true on success; false on failure)
    if (theResult == HttpStatusCode.OK) {  // *ALSO* VALID!

---
+++ DropNet/Models/Result.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DropNet.Models
+{
+    public class Result
+    {
+        public readonly System.Net.HttpStatusCode StatusCode;
+        
+        public static implicit operator bool(Result m)
+        {
+            // code to convert from Result to Boolean
+            return (m.StatusCode == System.Net.HttpStatusCode.OK);
+        }
+
+        public static implicit operator System.Net.HttpStatusCode(Result m)
+        {
+            // code to convert from Result to HttpStatusCode (for convenience and to keep dev code cleaner)
+            return m.StatusCode;
+        }
+
+        public Result(RestSharp.RestResponse m)
+        {
+            StatusCode = m.StatusCode;
+        }
+    }
+
+}
