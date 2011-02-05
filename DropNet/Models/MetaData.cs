24168893b420ce51fcc7d69aa8112faea413b1b6
kg4lod <jf@atlalabs.com>
2011-02-04 19:33:35 -0800

    Fixed multi-dev reported error of MetaData.Modified values being incorrect (always midnight of 1/1/1999).
    Problem: The problem is an incorrect cast inside RestSharpClient from String to DateTime.
    
    Fix:
    (1) Change MetaData.Modified to type: string so RestSharpClient returns the raw value
    (2) Added another field: MetaData.ModifiedDate of type DateTime which casts correctly. Developers can use this field to get the correct System.DateTime object.

--- DropNet/Models/MetaData.cs
+++ DropNet/Models/MetaData.cs
@@ -8,7 +8,7 @@ namespace DropNet.Models
         public string Hash { get; set; }
         public bool Thumb_Exists { get; set; }
         public long Bytes { get; set; }
-        public DateTime Modified { get; set; }
+        public string Modified { get; set; }
         public string Path { get; set; }
         public bool Is_Dir { get; set; }
         public bool Is_Deleted { get; set; }
@@ -16,6 +16,15 @@ namespace DropNet.Models
         public string Root { get; set; }
         public string Icon { get; set; }
         public List<MetaData> Contents { get; set; }
+
+        public System.DateTime ModifiedDate
+        {
+            get
+            {
+                //cast to datetime and return
+                return DateTime.Parse(Modified).Date;
+            }
+        }
 
         public string Name
         {
