dbc47de4717d6ff35b1a91d77049aaa6f7131cba
Jason MacEntee <jason@digitalmettle.com>
2011-03-30 05:40:17 +0800

    Changes made to support WP7 App DataHub
    Included the Dropbox concept of 'hash' for GetMetaData.
    Fixed the ability to List, Upload, and Delete files at the Dropbox root.
    Updated the MetaData class so the most important properties for sync could be
    Serialized (to IsolatedStorage)
    Added UTCDateModified for date comparison.
    Added the undocumented Revision property for direct numeric comparison.

--- DropNet/Models/MetaData.cs
+++ DropNet/Models/MetaData.cs
@@ -1,20 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace DropNet.Models
 {
+    [DataContract]
     public class MetaData
     {
+        [DataMember]
         public string Hash { get; set; }
         public bool Thumb_Exists { get; set; }
+        [DataMember]
         public long Bytes { get; set; }
+        [DataMember]
         public string Modified { get; set; }
+        [DataMember]
         public string Path { get; set; }
         public bool Is_Dir { get; set; }
         public bool Is_Deleted { get; set; }
+        [DataMember]
         public string Size { get; set; }
         public string Root { get; set; }
         public string Icon { get; set; }
+        [DataMember]
+        public int Revision { get; set; }
+        [DataMember]
         public List<MetaData> Contents { get; set; }
 
         public System.DateTime ModifiedDate
@@ -26,10 +36,28 @@ namespace DropNet.Models
             }
         }
 
+		 public DateTime UTCDateModified
+        {
+            get
+            {
+                string str = Modified;
+                if (str.EndsWith(" +0000")) str = str.Substring(0, str.Length - 6);
+                if (!str.EndsWith(" UTC")) str += " UTC";
+                return DateTime.ParseExact(str, "ddd, d MMM yyyy HH:mm:ss UTC", System.Globalization.CultureInfo.InvariantCulture);
+            }
+            set
+            {
+                Modified = value.ToString("ddd, d MMM yyyy HH:mm:ss UTC");
+            }
+        }
+
+		
+		
         public string Name
         {
             get
             {
+                if (string.IsNullOrEmpty(Path)) return string.Empty;
                 if (Path.LastIndexOf("/") == -1)
                 {
                     return string.Empty;
@@ -40,10 +68,12 @@ namespace DropNet.Models
                 }
             }
         }
+		
         public string Extension
         {
             get
             {
+                if (string.IsNullOrEmpty(Path)) return string.Empty;
                 if (Path.LastIndexOf(".") == -1)
                 {
                     return string.Empty;
