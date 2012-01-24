83100ca30acf947adab06ca6edec07d8a79f2561
dkarzon <dkarzon@tboda.com>
2012-01-25 09:48:15 +1000

    Added a web sample project.

---
+++ DropNet.Samples/DropNet.Samples.Web/Default.aspx
@@ -0,0 +1,12 @@
+﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.master" AutoEventWireup="true"
+    CodeBehind="Default.aspx.cs" Inherits="DropNet.Samples.Web._Default" %>
+
+<asp:Content ID="HeaderContent" runat="server" ContentPlaceHolderID="HeadContent">
+</asp:Content>
+<asp:Content ID="BodyContent" runat="server" ContentPlaceHolderID="MainContent">
+    <h2>
+        Click the button to start.
+    </h2>
+    <asp:Literal ID="litOutput" runat="server"></asp:Literal>
+    <asp:Button ID="btnStart" runat="server" Text="Start" OnClick="btnStart_Click" />
+</asp:Content>
---
+++ DropNet.Samples/DropNet.Samples.Web/Default.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DropNet.Samples.Web
+{
+    public partial class _Default : System.Web.UI.Page
+    {
+        ////////////////////////////////////////////////////
+        // NOTE: This key is a Development only key setup for this sample and will only work with my login.
+        // MAKE SURE YOU CHANGE IT OR IT WONT WORK!
+        ////////////////////////////////////////////////////
+        DropNetClient _client = new DropNetClient("9m6v782a7aeop0w", "dbd11uqce6hr8zg");
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Request.Params["dropboxcallback"] == "1")
+            {
+                //Its a callback from dropbox!
+                if (Session["DropNetUserLogin"] != null)
+                {
+                    _client.UserLogin = Session["DropNetUserLogin"] as DropNet.Models.UserLogin;
+                    Session["DropNetUserLogin"] = _client.GetAccessToken();
+
+                    var accountinfo = _client.Account_Info();
+                    litOutput.Text = accountinfo.quota_info.quota.ToString();
+                }
+                else
+                {
+                    litOutput.Text = "Session expired...";
+                }
+            }
+        }
+
+        protected void btnStart_Click(object sender, EventArgs e)
+        {
+            Session["DropNetUserLogin"] = _client.GetToken();
+
+            var url = _client.BuildAuthorizeUrl(Request.Url.ToString() + "?dropboxcallback=1");
+
+            Response.Redirect(url);
+        }
+    }
+}
