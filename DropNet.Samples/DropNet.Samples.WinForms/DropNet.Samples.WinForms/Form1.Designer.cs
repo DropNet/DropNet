55139993ba69d2b73561f5276e000c0c023664da
Damian Karzon <dkarzon@tboda.com>
2012-08-06 23:22:38 +1000

    Started on the WinForms Sample app.

---
+++ DropNet.Samples/DropNet.Samples.WinForms/DropNet.Samples.WinForms/Form1.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DropNet.Samples.WinForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_LoginEmbed = new System.Windows.Forms.Button();
+            this.brwLogin = new System.Windows.Forms.WebBrowser();
+            this.SuspendLayout();
+            // 
+            // btn_LoginEmbed
+            // 
+            this.btn_LoginEmbed.Location = new System.Drawing.Point(12, 12);
+            this.btn_LoginEmbed.Name = "btn_LoginEmbed";
+            this.btn_LoginEmbed.Size = new System.Drawing.Size(122, 61);
+            this.btn_LoginEmbed.TabIndex = 0;
+            this.btn_LoginEmbed.Text = "Login (Embedded Browser)";
+            this.btn_LoginEmbed.UseVisualStyleBackColor = true;
+            this.btn_LoginEmbed.Click += new System.EventHandler(this.btn_LoginEmbed_Click);
+            // 
+            // brwLogin
+            // 
+            this.brwLogin.Location = new System.Drawing.Point(12, 79);
+            this.brwLogin.MinimumSize = new System.Drawing.Size(20, 20);
+            this.brwLogin.Name = "brwLogin";
+            this.brwLogin.Size = new System.Drawing.Size(351, 391);
+            this.brwLogin.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(576, 482);
+            this.Controls.Add(this.brwLogin);
+            this.Controls.Add(this.btn_LoginEmbed);
+            this.Name = "Form1";
+            this.Text = "DropNet.Sample.WinForms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_LoginEmbed;
+        private System.Windows.Forms.WebBrowser brwLogin;
+    }
+}
+
---
+++ DropNet.Samples/DropNet.Samples.WinForms/DropNet.Samples.WinForms/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using DropNet.Models;
+
+namespace DropNet.Samples.WinForms
+{
+    public partial class Form1 : Form
+    {
+        private DropNetClient _client;
+        private UserLogin _userLogin;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            ////////////////////////////////////////////////////
+            // NOTE: This key is a Development only key setup for this sample and will only work with my login.
+            // MAKE SURE YOU CHANGE IT OR IT WONT WORK!
+            ////////////////////////////////////////////////////
+            _client = new DropNetClient("9m6v782a7aeop0w", "dbd11uqce6hr8zg");
+
+            //NOTE: If user Token and Secret are stored from previous login session:
+            //DropNetClient.UserLogin = new Models.UserLogin { Token = "TokenFromStorage", Secret = "SecretFromStorage" };
+        }
+
+        private void btn_LoginEmbed_Click(object sender, EventArgs e)
+        {
+            var authorizeUrl = _client.GetTokenAndBuildUrl("http://www.google.com.au");
+            
+            brwLogin.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(brwLogin_DocumentCompleted);
+            brwLogin.Navigate(authorizeUrl);
+
+
+            //Async
+
+            ////Get the Request Token then load the browser
+            //_client.GetTokenAsync((userLogin) =>
+            //    {
+            //        //Dont need to do anything here with userLogin if we keep the same instance of DropNetClient
+
+            //        //Now generate the url for the user to authorize the app
+            //        var tokenUrl = _client.BuildAuthorizeUrl("http://www.google.com.au"); //Use your own callback Url here
+
+            //        brwLogin.DocumentCompleted += new WebBrowserDocumentCompletedEventHandler(brwLogin_DocumentCompleted);
+            //        brwLogin.Navigate(tokenUrl);
+            //    },
+            //    (error) =>
+            //    {
+            //        //Some sort of error
+            //        MessageBox.Show(error.Response.Content);
+            //    });
+        }
+
+        private void brwLogin_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            if (e.Url.Host.Contains("google"))
+            {
+                //Callback url reached, user has completed authorization
+
+                //Now convert the request token into an access token
+                _client.GetAccessTokenAsync((userLogin) =>
+                    {
+                        //Save the Token and Secret we get here to save future logins
+                        _userLogin = userLogin;
+
+                        LoadContents();
+                    },
+                    (error) =>
+                    {
+                        //Some sort of error
+                        MessageBox.Show(error.Response.Content);
+                    });
+            }
+        }
+
+        private void LoadContents()
+        {
+            _client.GetMetaDataAsync("/", (response) =>
+            {
+                MessageBox.Show(response.Contents.Count(c => c.Is_Dir) + " Folders found.");
+            },
+            (error) =>
+            {
+                MessageBox.Show(error.Message);
+            });
+        }
+
+    }
+}
